61dd2fe343486ad810c5af239548f7d4e2dd0b55
Alexander Smirnov <kesher88@gmail.com>
2021-04-19 17:42:11 +0300

    Add animated toolbar

--- app/src/main/java/ru/alexkesh/leroymerlin/ui/fragments/MainFragment.kt
+++ app/src/main/java/ru/alexkesh/leroymerlin/ui/fragments/MainFragment.kt
@@ -1,9 +1,12 @@
 package ru.alexkesh.leroymerlin.ui.fragments
 
+import android.animation.ArgbEvaluator
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import ru.alexkesh.leroymerlin.R
@@ -35,9 +38,34 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        activity?.window?.statusBarColor = requireContext().getColor(R.color.brand_green)
+        setMotionLayoutListener()
+
         binding.rvGroups.adapter = GroupAdapter().also { it.submitList(viewModel.getGroups()) }
         binding.rvGroups.setHasFixedSize(true)
 
+    }
+
+    private fun setMotionLayoutListener() {
+        binding.mainMotionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, progress: Float) {
+                setStatusBarColor(progress)
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout, p1: Int) {
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+        })
+    }
+
+    private fun setStatusBarColor(progress: Float) {
+        val color = (binding.toolbar.background as? ColorDrawable)?.color
+        color?.let { activity?.window?.statusBarColor = it }
     }
 
     override fun onDestroyView() {
